035a1d042070cf7dd35fe3ebfd62a9427dbfb9c8
Balaji Bodkekar <bodkekarbalaji95@gmail.com>
2026-04-21 21:38:43 +0530

    Added unit test for App

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,12 @@
 package com.example;
 
 public class App {
+
+    public static String getMessage() {
+        return "Hello, GitHub Actions!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub Actions!");
+        System.out.println(getMessage());
     }
 }
